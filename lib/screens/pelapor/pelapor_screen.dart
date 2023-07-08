f568e05a88b4043f6253f3d4c2660072ae1b2e31
tomjerry <Andrialfiandi46@gmail.com>
2023-07-09 05:36:40 +0800

    09/07/2023

--- lib/screens/pelapor/pelapor_screen.dart
+++ lib/screens/pelapor/pelapor_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sistem_pelaporan/components/text/text_component.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:sistem_pelaporan/values/placeholders.dart';
 
 class PelaporScreen extends StatefulWidget {
   const PelaporScreen({super.key});
@@ -13,9 +14,37 @@ class _PelaporScreenState extends State<PelaporScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: TextComponent("Pelapor"),
-      ),
+      body: Shimmer.fromColors(
+          baseColor: Colors.grey.shade300,
+          highlightColor: Colors.grey.shade100,
+          enabled: true,
+          child: const SingleChildScrollView(
+            physics: NeverScrollableScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                BannerPlaceholder(),
+                TitlePlaceholder(width: double.infinity),
+                SizedBox(height: 16.0),
+                ContentPlaceholder(
+                  lineType: ContentLineType.threeLines,
+                ),
+                SizedBox(height: 16.0),
+                TitlePlaceholder(width: 200.0),
+                SizedBox(height: 16.0),
+                ContentPlaceholder(
+                  lineType: ContentLineType.twoLines,
+                ),
+                SizedBox(height: 16.0),
+                TitlePlaceholder(width: 200.0),
+                SizedBox(height: 16.0),
+                ContentPlaceholder(
+                  lineType: ContentLineType.twoLines,
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
